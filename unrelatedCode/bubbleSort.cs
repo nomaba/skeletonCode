87c310c3e35a6ae582ffd5211e2fcd5d0d017b41
nomaba <118121397+nomaba@users.noreply.github.com>
2025-03-13 06:41:40 +0000

    unrelated code

--- unrelatedCode/bubbleSort.cs
+++ unrelatedCode/bubbleSort.cs
@@ -1,6 +1,37 @@
-bool swapMade = true;
+public bool swapMade = true;
+public bool needSwap = false;
 
-while(swapMade == false)
+static void Main (string[] args)
 {
-    
+    while(swapMade == false)
+    {
+
+        swapMade = false;
+        for (int i = 0; i <= 9; i++)
+        {
+            compareItems(i);
+
+            if (needSwap == true)
+            {
+                swapItems(i);
+                swapMade = true;
+                needSwap = false;
+            }
+        }
+    }
+}
+
+static void swapItems(int i;)
+{
+    string temp;
+    array[whatSwap] = array[whatSwap + 1];
+    array[whatSwap + 1] = temp;
+}
+
+static void compareItems(int i;)
+{
+    if(array[i] > array[i+1])
+    {
+        needSwap = true;
+    }
 }
