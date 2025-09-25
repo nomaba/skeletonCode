4a68e568fbb042903b7db637b10bf2f165af5cf2
nomaba <118121397+nomaba@users.noreply.github.com>
2025-09-25 12:19:05 +0100

    created list of subroutines (not complete)

---
+++ commentedCode/allSubroutinesAndLocations.cs
@@ -0,0 +1,22 @@
+namespace AntSimCS
+{
+    class Program
+    {
+
+
+
+        static void Main()
+        static void DisplayMenu()
+        static string GetChoice()
+        static void GetCellReference(ref int Row, ref int Column)
+
+
+
+        class Simulation
+        {
+            public Simulation(List<int> SimulationParameters)
+            public void SetUpANestAt(int Row, int Column)
+            public void AddFoodToCell(int Row, int Column, int Quantity)
+        }
+    }
+}
